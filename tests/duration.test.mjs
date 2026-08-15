c1529c587ea0f1342b175b9ba9cb54156a06b46b
tiroq <tiroq@users.noreply.github.com>
2026-08-15 22:34:57 +0700

    test: add additional test case for formatDuration function

--- tests/duration.test.mjs
+++ tests/duration.test.mjs
@@ -4,6 +4,7 @@ import { formatDuration } from "../app/duration.js";
 
 test("formatDuration always includes hours, minutes, and seconds", () => {
   assert.equal(formatDuration(4093), "1 ч 08 мин 13 сек");
+  assert.equal(formatDuration(67758), "18 ч 49 мин 18 сек");
   assert.equal(formatDuration(61), "0 ч 01 мин 01 сек");
   assert.equal(formatDuration(0), "0 ч 00 мин 00 сек");
 });
